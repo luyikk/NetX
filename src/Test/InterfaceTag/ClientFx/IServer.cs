0c63b61290ccc820aeb10584230f6b85ce435c3b
luyikk <luyikk@126.com>
2019-08-08 01:32:26 +0800

    FIX WINFROM CALL

---
+++ src/Test/InterfaceTag/ClientFx/IServer.cs
@@ -0,0 +1,37 @@
+﻿using Netx;
+using System;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    [Build]
+    public interface IServerOld
+    {
+        [TAG(1001)]
+        Task<int> AddOne(int a);
+    }
+
+    [Build]
+    public interface IServer: IServerOld
+    {
+        [TAG(1000)]
+        Task<int> Add(int a, int b);
+    }
+
+    [Build]
+    public interface IServerNew
+    {
+        [TAG(1002)]
+        Task<int> AddTow(int a);
+
+        [TAG(1003)]
+        void Run(string msg);
+    }
+
+    [Build]
+    public interface IServerDef
+    {
+        [TAG(2000)]
+        Task<int> Add3(int a);
+    }
+}
