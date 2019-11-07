561ca533cfdb015087cd42386337dc0d153b1c94
yi lu <luyikk@126.com>
2019-11-07 14:58:36 +0800

    add initialization

---
+++ src/NetxServer/Initialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Netx.Service
+{
+    /// <summary>
+    /// start install
+    /// </summary>
+    public interface Initialization
+    {
+        void initialize();
+    }
+}
